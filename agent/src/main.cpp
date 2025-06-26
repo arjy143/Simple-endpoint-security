5782d0e4f5f5caff74f3c3a3d770c0b6bff5a85a
Arjun <arjun_p_uk@yahoo.co.uk>
2025-06-26 17:52:30 +0100

    simulates logging telemetry data

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -1,17 +1,28 @@
 #include <windows.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
+void LogData(){
+    for (int i = 0; i < 10; i++){
+        MEMORYSTATUSEX memStatus;
+        SYSTEM_INFO sysInfo;
+        GetSystemInfo(&sysInfo);
+        printf("oem id: %u\n", sysInfo.dwOemId);
+        printf("no of processors: %d\n", sysInfo.dwNumberOfProcessors);
+        printf("page size: %u\n", sysInfo.dwPageSize);
+        printf("Processor type: %u\n", sysInfo.dwProcessorType); 
+        printf("Minimum application address: %lx\n", sysInfo.lpMinimumApplicationAddress); 
+        printf("Maximum application address: %lx\n", sysInfo.lpMaximumApplicationAddress); 
+        printf("Active processor mask: %u\n", sysInfo.dwActiveProcessorMask); 
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    
+}
 int main(){
-    MEMORYSTATUSEX memStatus;
-    SYSTEM_INFO sysInfo;
-
-    GetSystemInfo(&sysInfo);
-    printf("oem id: %u\n", sysInfo.dwOemId);
-    printf("no of processors: %d\n", sysInfo.dwNumberOfProcessors);
-    printf("page size: %u\n", sysInfo.dwPageSize);
-    printf("Processor type: %u\n", sysInfo.dwProcessorType); 
-    printf("Minimum application address: %lx\n", sysInfo.lpMinimumApplicationAddress); 
-    printf("Maximum application address: %lx\n", sysInfo.lpMaximumApplicationAddress); 
-    printf("Active processor mask: %u\n", sysInfo.dwActiveProcessorMask); 
-
+    
+    std::thread second(LogData);
+    second.join();
+    std::cin.get();
 }
